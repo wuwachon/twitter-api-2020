a7058a91d6683adf92a535d64380b3fadc4fd29b
wuwac <wuwachon44@gmail.com>
2022-07-28 02:07:40 +0800

    feat: add routes files

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
 const express = require('express')
 const router = express.Router()
 
-const APIController = require('../controllers/APItest-controller')
+const APItestController = require('../controllers/APItest-controller')
+const userController = require('../controllers/user-controller')
+const adminController = require('../controllers/admin-controller')
 
-router.get('/api/test', APIController.getTestJSON)
+const admin = require('./modules/admin')
+const users = require('./modules/users')
+const tweets = require('./modules/tweets')
+const followships = require('./modules/followships')
+const passport = require('../config/passport')
+
+const { authenticated, authenticateUser, authenticateAdmin } = require('../middlewares/api-auth')
+const { apiErrorHandler } = require('../middlewares/error-handler')
+
+router.get('/api/test', APItestController.getTestJSON)
+router.post('/api/admin/signin', passport.authenticate('local', { session: false }), authenticateAdmin, adminController.signIn)
+router.post('/api/users/signin', passport.authenticate('local', { session: false }), authenticateUser, userController.signIn)
+router.post('/api/users', userController.signUp)
+
+router.use('/api/admin', authenticated, authenticateAdmin, admin)
+router.use('/api/users', authenticated, authenticateUser, users)
+router.use('/api/tweets', authenticated, authenticateUser, tweets)
+router.use('/api/followships', authenticated, authenticateUser, followships)
+
+router.use('/api', apiErrorHandler)
 
 module.exports = router
